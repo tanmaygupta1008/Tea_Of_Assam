2f07fe43896fa5f3a6af4f16d3182e68782a2369
Tanmay <nidhideepjyoti@student.sfit.ac.in>
2025-03-24 19:21:17 +0530

    Initial commit

---
+++ src/components/ServiceworkerRegistration.jsx
@@ -0,0 +1,7 @@
+// self.addEventListener('install', (event) => {
+//     console.log('Service Worker installing...');
+//     self.skipWaiting();
+// });
+// self.addEventListener('activate', (event) => {
+//     console.log('Service Worker activating...');
+//  });
