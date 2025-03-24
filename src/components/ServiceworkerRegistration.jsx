08e6fe5048566b454f7ab59c1efadecb7036b4f9
tanmaygupta1008 <guptatanmay1008@gmail.com>
2025-03-24 21:23:15 +0530

    Third Commit

--- src/components/ServiceworkerRegistration.jsx
+++ src/components/ServiceworkerRegistration.jsx
@@ -1,7 +1,15 @@
-// self.addEventListener('install', (event) => {
-//     console.log('Service Worker installing...');
-//     self.skipWaiting();
-// });
-// self.addEventListener('activate', (event) => {
-//     console.log('Service Worker activating...');
-//  });
+export function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+    });
+  } else {
+    console.log('Service Workers are not supported in this browser.');
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,47 +3,27 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
-import { registerServiceWorker } from "./components/ServiceworkerRegistration.jsx";
+import { registerServiceWorker } from "./components/serviceWorkerRegistration.jsx"; 
+
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
+      navigator.serviceWorker.register('/sw.js')
+          .then((registration) => {
+              console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+              console.log('Service Worker registration failed:', error);
+          });
   });
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <React.StrictMode>
- <BrowserRouter>
- <App />
- </BrowserRouter>
- </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 registerServiceWorker();
-
-
-
-
-
-
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./styles/index.css";
-// import { BrowserRouter } from "react-router-dom";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// );
