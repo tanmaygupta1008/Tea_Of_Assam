936afedfb3a51276b79d5134278976fcd65c6a9f
tanmaygupta1008 <guptatanmay1008@gmail.com>
2025-04-06 12:30:31 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/assets/account.png', // Add other assets you want to cache
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installing...');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activating...');
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
